272221aad2412495314b43cd0b420acc4a2b4013
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-05-19 00:00:36 +0100

    .

--- Aula_3/JavaApplication6/src/javaapplication6/Exercicio_103.java
+++ Aula_3/JavaApplication6/src/javaapplication6/Exercicio_103.java
@@ -14,22 +14,27 @@ public class Exercicio_103 {
 
    
     public static void main(String args[]) {
-       int x,y,z;
+       double x,y,z;
         Scanner in= new Scanner(System.in);
         System.out.println("Insira os seus tres numeros: ");
-        x=in.nextInt();
-        y=in.nextInt();
-        z=in.nextInt();
+        x=in.nextDouble();
+        y=in.nextDouble();
+        z=in.nextDouble();
         
         if(x>y && x>z){
-            System.out.println(x);}
+            System.out.println("O maior número é o primeiro: "+x);
+        }
             else if(y>x && y>z){
-                System.out.println(y);
-                else{
-                       
+                System.out.println("O maior número é o segundo: "+y);
+            }
+            else if(x==y&&x==z){
+                    System.out.println("Os seus numeros sao iguais");        
+                }
+            else {
+                       System.out.println("O maior número é o terceiro: " + z);
                         }
             
         }
         
     }
-}
+
