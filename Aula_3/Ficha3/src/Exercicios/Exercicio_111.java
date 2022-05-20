e7ad324f2baac4d9595259beb334a0a6f574ae80
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-05-20 11:56:54 +0100

    ficha3

---
+++ Aula_3/Ficha3/src/Exercicios/Exercicio_111.java
@@ -0,0 +1,11 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_111{
+    
+    public static void main(String [] args){
+    
+    Scanner in= new Scanner(System.in);
+    }
+}
