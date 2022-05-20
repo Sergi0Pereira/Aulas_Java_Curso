8d18b003651bf995f923ff9b59a46596e46d8f48
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-05-20 11:56:19 +0100

    ficha3

--- Aula_2/Ficha_2/JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ Aula_2/Ficha_2/JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -2,24 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ficha_2;
-import java.util.Scanner;
+package javaapplication9;
+
 /**
  *
  * @author sergio
  */
-public class Exercicio_101 {
+public class JavaApplication9 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String nome;
-        System.out.println("Insira por favor o seu nome:");
-        Scanner in= new Scanner(System.in);
-        nome=in.next();
-        System.out.println("Olá "+nome);
-             
         // TODO code application logic here
     }
     
