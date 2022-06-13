eba0d6a2625397b1b76dc8822cf2c7f25a2e0a53
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-06-13 01:31:14 +0100

    update

--- Alunos.java
+++
@@ -1,9 +0,0 @@
-class Alunos {
-
-    String caloiro;
-    String doutor;
-
-    
-
-         
-}
--- Estudo/Metodos.java
+++
@@ -1,9 +0,0 @@
-package Estudo;
-
-public class Metodos {
-    public void name() {
-
-        System.out.println("Ola");
-
-    }
-}
